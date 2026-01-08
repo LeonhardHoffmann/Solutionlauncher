a26f911ae8da2ad7af9d5577c504460644b440bb
Leonhard <leonhard.hoffmann8@gmail.com>
2026-01-08 10:03:53 +0100

    style: change style of settings heading

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,7 @@
 export default function Settings() {
   return (
     <div className="w-screen">
-      <h1 className="flex justify-center w-full">test</h1>
+      <h1 className="flex justify-center w-full font-bold text-4xl">Settings</h1>
     </div>
   );
 }
