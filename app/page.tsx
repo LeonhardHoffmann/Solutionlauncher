28790a8121678e8c2a6b9626086bf2b56abba1a4
Leonhard <leonhard.hoffmann8@gmail.com>
2026-01-08 15:33:57 +0100

    feat: create first page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-
+      <div className="w-screen">
+        <h1 className="text-primary flex justify-center w-full font-bold text-7xl">
+          Create new Project
+        </h1>
+        <section className="my-5 mx-[300] border-3 rounded-xl p-10"></section>
+      </div>
     </>
   );
 }
