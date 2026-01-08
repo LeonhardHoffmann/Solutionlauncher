8ca93d3623ad505693e1e7c6b6fe88d7668fb954
Leonhard <leonhard.hoffmann8@gmail.com>
2026-01-08 11:08:30 +0100

    refactor: remove unused imports

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,19 +1,8 @@
 "use client";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { Sun, Moon } from "lucide-react";
-import { useTheme } from "next-themes";
 import ThemeSwitcher from "@/components/theme-switcher-02";
 
 export default function Settings() {
-  const { setTheme } = useTheme();
-
   return (
     <>
       <div className="w-screen">
@@ -21,12 +10,10 @@ export default function Settings() {
           Settings
         </h1>
       </div>
-      <div className="border-1 my-5 mx-8">
-        <section>
-          <h2 className="font-bold text-2xl">Color theme</h2>
-          <ThemeSwitcher></ThemeSwitcher>
-        </section>
-      </div>
+      <section className="my-5 mx-8 border-3 rounded-xl p-10">
+        <h2 className="font-bold text-2xl">Color theme</h2>
+        <ThemeSwitcher></ThemeSwitcher>
+      </section>
     </>
   );
 }
