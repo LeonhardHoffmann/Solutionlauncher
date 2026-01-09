7cf7f12114c0d12995e8974f49c9444c41e97cac
Leonhard Hoffmann <Leonhard.hoffmann8@gmail.com>
2026-01-09 21:56:16 +0100

    feat: add onselect to techbox component

--- components/techbox.tsx
+++ components/techbox.tsx
@@ -3,7 +3,7 @@ interface TechboxProps {
     logoUrl: string;
     onSelect?: () => void;
 }
-export default function Techbox({ name, logoUrl }: TechboxProps) {
+export default function Techbox({ name, logoUrl, onSelect}: TechboxProps) {
 
     return (
         <div className="flex flex-col justify-center items-center gap-4 w-30 h-30 border rounded-xl hover:bg-gray-100 cursor-pointer">
